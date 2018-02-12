d08524bec4f30bbba9776034f9d5c938c44d74eb
J/A <archer884@gmail.com>
2018-02-12 10:29:20 -0600

    Benchmarks

---
+++ benches/convert.rs
@@ -0,0 +1,29 @@
+#![feature(test)]
+extern crate test;
+
+extern crate xvii;
+
+use test::Bencher;
+use xvii::Roman;
+
+#[bench]
+fn convert_from(b: &mut Bencher) {
+    let values: Vec<_> = (100..501).map(|n| Roman::from_unchecked(n).to_string()).collect();
+    
+    b.iter(|| {
+        for value in &values {
+            test::black_box(value.parse::<Roman>());
+        }
+    });
+}
+
+#[bench]
+fn format(b: &mut Bencher) {
+    let values: Vec<_> = (100..501).collect();
+
+    b.iter(|| {
+        for &value in &values {
+            test::black_box(Roman::from_unchecked(value).to_string());
+        }
+    })
+}
