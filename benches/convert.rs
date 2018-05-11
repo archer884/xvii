a325f320fde2eb64e3886eab19a07ba51e93929a
J/A <archer884@gmail.com>
2018-05-11 17:37:04 -0500

    More useful benchmarks

--- benches/convert.rs
+++ benches/convert.rs
@@ -7,25 +7,13 @@ use test::Bencher;
 use xvii::Roman;
 
 #[bench]
-fn convert_from(b: &mut Bencher) {
-    let values: Vec<_> = (100..501)
-        .map(|n| Roman::from_unchecked(n).to_string())
-        .collect();
-
-    b.iter(|| {
-        for value in &values {
-            test::black_box(value.parse::<Roman>().ok());
-        }
-    });
+fn convert_mmmdcccxciii(b: &mut Bencher) {
+    let value = "mmmmdcccxciiii";
+    b.iter(|| test::black_box(value.parse::<Roman>()));
 }
 
 #[bench]
 fn format(b: &mut Bencher) {
-    let values: Vec<_> = (100..501).collect();
-
-    b.iter(|| {
-        for &value in &values {
-            test::black_box(Roman::from_unchecked(value).to_string());
-        }
-    })
+    let value = 4894;
+    b.iter(|| test::black_box(Roman::from_unchecked(value).to_string()));
 }
